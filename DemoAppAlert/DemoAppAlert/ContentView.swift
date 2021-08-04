eff9dd98640828fe7968c533f0542d5d80e9dd9e
Pravin Agravat <pravin@nium.com>
2021-08-04 16:17:17 +0530

    StartDemo

---
+++ DemoAppAlert/DemoAppAlert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoAppAlert
+//
+//  Created by IOSDEV1 on 04/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoAppAlert/DemoAppAlert/DemoAppAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppAlertApp.swift
+//  DemoAppAlert
+//
+//  Created by IOSDEV1 on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
