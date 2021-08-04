202e50138b8f2c299f26fe27d8075db56c2f358f
Pravin Agravat <pravin@nium.com>
2021-08-04 16:55:15 +0530

    demoAlert

--- DemoAppAlert/DemoAppAlert/ContentView.swift
+++ DemoAppAlert/DemoAppAlert/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertType: AlertType? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button("Title Only") {
+                alertType = .ok(title: "This is Title Only")
+            }
+            Button("Title and Message") {
+                alertType = AlertType.ok(title: "Title Here",
+                                    message: "This is a message")
+            }
+            Button("Single Button (Title and Message)") {
+                alertType = AlertType.singleButton(title: "Congratulations",
+                                                message: "Process Completed",
+                                                dismissButton: Alert.Button.default(Text("OK")) {
+                                                    print("Ok")
+                                                })
+            }
+            Button("Two Buttons (no message)") {
+                alertType = AlertType.twoButtons(title: "Delete Profile",
+                                            primaryButton: .destructive(Text("OK")) {
+                                                print("primaryButton")
+                                            },
+                                            secondaryButton: .cancel())
+                           
+            }
+            
+        }
+        .alert(item: $alertType) {
+            $0.alert
+        }
+        .buttonStyle(GreenButtonStyle())
     }
 }
 
